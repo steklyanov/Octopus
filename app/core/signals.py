cbad1620cda9c0de01066f460250e931fa4a632c
Max Steklyanov <steklyanov@gmail.com>
2020-02-11 17:28:20 +0300

    add frontend part with docker

--- app/core/signals.py
+++ app/core/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from .models import Booker, Schedule
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 
 def create_booker(sender, instance, **kwargs):
@@ -9,13 +10,14 @@ def create_booker(sender, instance, **kwargs):
     booker = Booker.objects.create(user=instance, schedule=schedule)
 
 
-# def create_schedule(sender, instance, **kwargs):
-#     print("sender = ", sender)
-#     print("inst = ", instance)
-#     print("create schedule")
-#
-#     instance.schedule = schedule
+def create_user(sender, instance, **kwargs):
+    # user = User.objects.create()
+    print("sender = ", sender)
+    print("inst = ", instance)
+    print("create schedule")
+
+    # instance.schedule = schedule
 
 
 post_save.connect(create_booker, sender=get_user_model())
-# post_save.connect(create_schedule, sender=Booker)
+post_save.connect(create_user, sender=Booker)
--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import render
+
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.views import APIView
+from djoser.serializers import UserCreateSerializer
 from core.models import Actor, Client, Job, Booker, Agency
 from core.serializers import AgencySerializer, BookerSerializer
 from rest_framework import generics
 
 
 # class DashboardView(generics.RetrieveUpdateDestroyAPIView):
-class DashboardView(generics.ListAPIView):
+class DashboardView(generics.ListCreateAPIView):
     """ Show all agencies """
     serializer_class = AgencySerializer
 
     def get_queryset(self):
         return Agency.objects.all()
+
+
+# class BookerListCreateView(generics.ListCreateAPIView):
+#     """ Create or show all bookers of the selected agency """
+#     serializer_class = BookerSerializer
+#
+#     def get_queryset(self, *args, **kwargs):
+#         id = self.kwargs['pk']
+#         booker = Booker.objects.filter(agency_id=id)
+#
+#         return booker
 
 
 class BookerListCreateView(generics.ListCreateAPIView):
